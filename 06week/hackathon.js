ce0ad7b2d41745c63209476f34a4628728b3289a
winkleman <jwinks18@gmail.com>
2019-10-18 09:31:19 -0500

    Finished the higher order assignment

---
+++ 06week/hackathon.js
@@ -0,0 +1,49 @@
+// hang man _____
+
+var compChoice = ['h', 'e', 'l', 'l', 'o'];
+var userGuest = [];
+
+
+function playGame(guess){
+    for(i=0;i<computerChoice.length;i++){
+        if (guess == computerChoice[i]){
+            playSpace.push(guess);
+            console.log(playSpace);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 06week/higherOrder.js
+++ 06week/higherOrder.js
@@ -1,103 +1,105 @@
 'use strict';
 
-const assert = require('assert');
+let assert = require('assert');
 
-function forEach(arr, callback) {
-  // Your code here
+let jobTypes = {
+  pilot: 'MAV',
+  mechanic: 'Repair Ship',
+  commander: 'Main Ship',
+  programmer: 'Any Ship!'
+};
+
+
+//describing the crewmember object
+class CrewMember {
+  constructor(iname, ijob, ispecialSkill) {
+    this.name = iname;
+    this.job = ijob;
+    this.specialSkill = ispecialSkill;
+  }
+
+
+  //pushes the crewmemeber into the ship
+  enterShip(theShip) {
+    this.ship = theShip;
+    theShip.crew.push(this)
+
+  }
 }
 
-function map(arr, callback) {
-  // Your code here
+
+// describeing the ship
+class Ship {
+  constructor(isname, istype, isability) {
+    this.name = isname;
+    this.type = istype;
+    this.ability = isability;
+  }
+  
+  
+  missionStatement() {
+    if (this.crew.length >= 1) {
+      return this.ability
+    } else {
+      return "Can't perform a mission yet.";
+    }
+  }
 }
 
-function filter(arr, callback) {
-  // Your code here
-}
 
-function some(arr, callback) {
-  // Your code here
-}
 
-function every(arr, callback) {
-  // Your code here
-}
 
+
+let crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
+let crewMember2 = new CrewMember('Commander Lewis', 'commander', 'geology');
+
+let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
+
+
+
+//tests
 if (typeof describe === 'function') {
+  describe('CrewMember', function () {
+    it('should have a name, a job, a specialSkill and ship upon instantiation', function () {
+      var crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
+      assert.equal(crewMember1.name, 'Rick Martinez');
+      assert.equal(crewMember1.job, 'pilot');
+      assert.equal(crewMember1.specialSkill, 'chemistry');
+      assert.equal(crewMember1.ship, null);
+    });
 
-  describe('#forEach()', () => {
-    it('should call the callback the array.length number of times', () => {
-      let count = 0;
-      forEach([1, 2, 3], () => {
-        count++;
-      });
-      assert.equal(count, 3);
+    it('can enter a ship', function () {
+      let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
+      let crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
+      crewMember1.enterShip(mav);
+      assert.equal(crewMember1.ship, mav);
+      assert.equal(mav.crew.length, 1);
+      assert.equal(mav.crew[0], crewMember1);
     });
   });
 
-  describe('#map()', () => {
-    const arr = [1, 2, 3];
-    const mapped = map(arr, (num) => {
-      return num * num;
+  describe('Ship', function () {
+    it('should have a name, a type, an ability and an empty crew upon instantiation', function () {
+      let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
+      assert.equal(mav.name, 'Mars Ascent Vehicle');
+      assert.equal(mav.type, 'MAV');
+      assert.equal(mav.ability, 'Ascend into low orbit');
+      assert.equal(mav.crew.length, 0);
     });
-    it('should return new array with mapped items', () => {
-      assert.deepEqual(mapped, [1, 4, 9]);
-    });
-    it('should not affect the original array', () => {
-      assert.deepEqual(arr, [1, 2, 3]);
-    })
-  });
 
-  describe('#filter()', () => {
-    it('should return an array of items that pass the predicate test', () => {
-      const filtered = filter([1, 2, 3], (num) => {
-        return num % 2 === 0;
-      });
-      assert.deepEqual(filtered, [2]);
-    });
-  });
+    it('can return a mission statement correctly', function () {
+      let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
+      let crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
+      let hermes = new Ship('Hermes', 'Main Ship', 'Interplanetary Space Travel');
+      let crewMember2 = new CrewMember('Commander Lewis', 'commander', 'geology');
+      assert.equal(mav.missionStatement(), "Can't perform a mission yet.");
+      assert.equal(hermes.missionStatement(), "Can't perform a mission yet.");
 
-  describe('#some()', () => {
-    let count = 0;
-    const somed = some([1, 2, 3, 4], (num) => {
-      count++;
-      return num % 2 === 0;
-    });
-    it('should return true if at least one item passes the predicate test', () => {
-      assert.equal(somed, true);
-    });
-    it('should stop at the first item that passes the predicate test', () => {
-      assert.equal(count, 2);
-    });
-    it('should return false if no items pass the predicate test', () => {
-      const somed = some([1, 3, 5], (num) => {
-        return num % 2 === 0;
-      });
-      assert.equal(somed, false);
+      crewMember1.enterShip(mav);
+      assert.equal(mav.missionStatement(), "Ascend into low orbit");
+
+      crewMember2.enterShip(hermes);
+      assert.equal(hermes.missionStatement(), "Interplanetary Space Travel");
     });
   });
-
-  describe('#every()', () => {
-    it('should return true if at all passes the predicate test', () => {
-      const everied = every([2, 4, 6], (num) => {
-        return num % 2 === 0;
-      });
-      assert.equal(everied, true);
-    });
-    let count = 0;
-    const everied = every([2, 3, 4, 5], (num) => {
-      count++;
-      return num % 2 === 0;
-    });
-    it('should return false if any item fails the predicate test', () => {
-      assert.equal(everied, false);
-    });
-    it('should stop at the first item that fails the predicate test', () => {
-      assert.equal(count, 2);
-    });
-  });
-
-} else {
-
-  console.log('Only run the tests on this one!')
-
 }
